cf37c0902e32b1a88388e5cf802219c601543e58
esaki01 <social0430@gmail.com>
2019-04-20 14:13:15 +0900

    Modify comment and variable name

--- src/example/search/LinearSearch.java
+++ src/example/search/LinearSearch.java
@@ -1,23 +1,23 @@
 /*
-線形探索法.
+線形探索法（リニアサーチ）.
 先頭から順番に調べて探す探索アルゴリズム.
  */
 package example.search;
 
 public class LinearSearch {
-    public static void main(String[] args){
-        int[] array = {4, 2, 3, 5, 1};
-        int value = 5;
-        search(array, value);
+    public static void main(String[] args) {
+        int[] data = {4, 2, 3, 5, 1};
+        int target = 5;
+        search(data, target);
     }
 
-    private static void search(int[] array, int value){
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == value) {
-                System.out.printf("%d番目の要素に%dが格納されています.", i, value);
+    private static void search(int[] data, int target) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
+                System.out.printf("%d番目の要素に%dが格納されています.", i, target);
                 return;
             }
         }
-        System.out.printf("%dは見つかりませんでした.", value);
+        System.out.printf("%dは見つかりませんでした.", target);
     }
 }
