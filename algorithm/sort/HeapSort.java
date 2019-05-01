3a6abe1e4504b6db305fbe335a4c3a7592f7023f
esaki01 <social0430@gmail.com>
2019-05-01 19:33:49 +0900

    Implement heap sort

---
+++ algorithm/sort/HeapSort.java
@@ -0,0 +1,64 @@
+/*
+ヒープソート.
+選択ソートの応用的なアルゴリズム.
+ヒープ（親の値が子の値以上であるという条件を満たす完全二分木）の特性をうまく利用してソートを行う.
+ヒープから最大値である根を取り出す作業を繰り返し、その過程で取り出した値を並べていけば整列済みの配列が完成する.
+
+POINT
+ヒープを上左から順番に配列に格納すると、親のインデックスと子のインデックスの間には、以下の関係が成立する.
+- 任意の要素data[i]に対して：
+  - 親はdata[(i - 1) / 2]
+  - 左の子はdata[i * 2 + 1]
+  - 右の子はdata[i * 2 + 2]
+ */
+package sort;
+
+import sort.abstract_object.AbstractSort;
+
+public class HeapSort extends AbstractSort {
+
+    HeapSort(int[] data) {
+        super(data);
+    }
+
+    @Override
+    public void sort() {
+        // data[i] 〜 data[data.length - 1]をヒープ化
+        for (int i = (data.length - 1) / 2; i >= 0; i--) {
+            downHeap(i, data.length - 1);
+        }
+
+        for (int i = data.length - 1; i > 0; i--) {
+            // 最大要素と未ソート部末尾要素を交換
+            int tmp = data[0];
+            data[0] = data[i];
+            data[i] = tmp;
+
+            // data[0] 〜 data[i - 1]をヒープ化
+            downHeap(0, i - 1);
+        }
+    }
+
+    /**
+     * data[left] 〜 data[right]をヒープ化
+     *
+     * @param left 左端の要素のインデックス
+     * @param right 右端の要素のインデックス
+     */
+    private void downHeap(int left, int right) {
+        int root = data[left]; // 根
+        int child; // 大きいほうの子
+        int parent; // 親
+
+        for (parent = left; parent < (right + 1) / 2; parent = child) {
+            int cl = parent * 2 + 1; // 左の子
+            int cr = cl + 1; // 右の子
+            child = (cr <= right && data[cr] > data[cl]) ? cr : cl; // 大きいほう
+            if (root >= data[child]) {
+                break;
+            }
+            data[parent] = data[child];
+        }
+        data[parent] = root;
+    }
+}
---
+++ test/sort/HeapSortTest.java
@@ -0,0 +1,19 @@
+package sort;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapSortTest {
+
+    @Test
+    void sort() {
+        int[] data = {22, 5, 11, 32, 120, 68, 70};
+        int[] expected = {5, 11, 22, 32, 68, 70, 120};
+
+        HeapSort heapSort = new HeapSort(data);
+        heapSort.sort();
+
+        assertArrayEquals(expected, data);
+    }
+}
